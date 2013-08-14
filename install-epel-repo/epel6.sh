4a9eff49f0f861dac44440d18686bf238905f49b
root <root@localhost.localdomain>
2013-08-15 00:40:42 +0200

    n

--- install-epel-repo/epel6.sh
+++ install-epel-repo/epel6.sh
@@ -1,3 +1,5 @@
+rm -f remi-release-6*.rpm epel-release-6*.rpm
 wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
 rpm -Uvh remi-release-6*.rpm epel-release-6*.rpm
+rm -f remi-release-6*.rpm epel-release-6*.rpm
